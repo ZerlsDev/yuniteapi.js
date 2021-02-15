714ea500bc9ee5d36973e22344776b1daa6b1ec2
ZerlsDev <62802943+ZerlsDev@users.noreply.github.com>
2021-02-15 16:55:56 +0000

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    domain: 'yunite.xyz/api/v3',
+    endpoints: {
+        registration: '/guild/:guildId/registration/links',
+        block_user: '/guild/:guildId/registration/blocks',
+        custom_games: '/guild/:guildId/customs/current/join/:userId',
+        list_tournaments: '/guild/:guildId/tournaments',
+        tournaments_leaderboards: '/guild/:guildId/tournaments/:tournamentId/leaderboard',
+        tournament_matches: '/guild/:guildId/tournaments/:tournamentId/matches',
+        single_tournament_leaderboard: '/guild/:guildId/tournaments/:tournamentId/matches/:sessionId'
+    }   
+};
