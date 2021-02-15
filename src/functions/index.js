8339188433034c4c9bfab3632142efd59f0458d9
ZerlsDev <62802943+ZerlsDev@users.noreply.github.com>
2021-02-15 16:56:10 +0000

    Create index.js

---
+++ src/functions/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    registrationData: require('./registrationData.js'),
+    blockUser: require('./blockUser.js'),
+    blockEpic: require('./blockEpic.js'),
+    unblockUser: require('./unblockUser.js'),
+    unblockEpic: require('./unblockEpic.js'),
+    addPlayerToQueue: require('./addPlayerToQueue.js'),
+    listTournaments: require('./listTournaments.js'),
+    tournamentLeaderboards: require('./tournamentLeaderboards.js'),
+    tournamentMatches: require('./tournamentMatches.js'),
+    sessionLeaderboard: require('./sessionLeaderboard.js')
+};
